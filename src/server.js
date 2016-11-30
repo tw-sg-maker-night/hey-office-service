82e3e01eb8b608fd5352d12296780bc0fa2bd88a
Stephan Dowding <stephan.dowding@gmail.com>
2016-11-30 23:52:35 +0800

    adjusting for use with elastic beanstalk

--- src/server.js
+++ src/server.js
@@ -13,6 +13,8 @@ app.post('/', (req, res) => {
   res.json(apiAIresponse)
 })
 
-app.listen(3000, () => {
-  console.log('Say "Hey Office!" to port 3000!')
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Say "Hey Office!" to port ${port}!`)
 })
