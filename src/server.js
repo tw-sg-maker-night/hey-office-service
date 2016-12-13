c86c942c4b58710afb7cf2e8857b92fa2ee89bc9
Albert Salim <asalim@thoughtworks.com>
2016-12-13 21:15:02 +0800

    Respond to give_present action

--- src/server.js
+++ src/server.js
@@ -1,25 +1,33 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-  const data = req.body.originalRequest.data
+  console.log(req);
+
+  let response = ''
+
+  if (req.body.result.action === 'give_present') {
+    response = `Yes master Bruce, I'd buy you a ${req.body.result.parameters.item} for Christmas`
+  } else {
+    response = `Yes master Bruce! You said ${req.body.result.resolvedQuery}`
+  }
 
   let apiAIresponse = {
-    speech: `Yes master Bruce! You said ${data.text}`,
-    displayText: `Yes master Bruce! You said ${data.text}`,
+    speech: response,
+    displayText: response,
     data: {},
     contextOut: [],
     source: 'Somewhere only we know'
-  }
-  res.json(apiAIresponse)
-})
+  };
+  res.json(apiAIresponse);
+});
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Say "Hey Office!" to port ${port}!`)
-})
+});
